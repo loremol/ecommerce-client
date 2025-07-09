89bdf82ead2c2a444b77e8700fb72f24ce10df14
Lorenzo <cocco1805@gmail.com>
2025-07-10 00:34:03 +0200

    fixed stats

--- client.js
+++ client.js
@@ -123,6 +123,9 @@ async function populateProfile() {
 }
 
 function formatDateToYYYYMMDD(date) {
+    if (date === null)
+        return '';
+
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
