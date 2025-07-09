893f77c91e43ff9d0e4bd993c4382191fdc752a6
Lorenzo <cocco1805@gmail.com>
2025-07-10 00:14:39 +0200

    proper date formatting

--- client.js
+++ client.js
@@ -122,12 +122,20 @@ async function populateProfile() {
     }
 }
 
+function formatDateToYYYYMMDD(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+
 async function updateProfile() {
     const username = document.getElementById('updUsername').value;
     const email = document.getElementById('updEmail').value;
     const phone = document.getElementById('updPhone').value;
     const address = document.getElementById('updAddress').value;
-    const date_of_birth = new Date(document.getElementById('updDateOfBirth').value);
+    const date_of_birth = formatDateToYYYYMMDD(new Date(document.getElementById('updDateOfBirth').value));
 
     try {
         const response = await fetch(`${API_ENDPOINT}/auth/update/`, {
