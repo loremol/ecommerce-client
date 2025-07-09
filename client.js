3e2ee68e7b77be74fe10479c7a6c6f1943a4c431
Lorenzo <cocco1805@gmail.com>
2025-07-10 00:36:23 +0200

    fixed stats

--- client.js
+++ client.js
@@ -129,7 +129,10 @@ function formatDateToYYYYMMDD(date) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
+    const formattedDate = `${year}-${month}-${day}`;
+    if (formattedDate.includes("NaN"))
+        return '';
+    else return formattedDate;
 }
 
 
